bedf716f35166eaaf5213e5237b3b44837ac42ea
luochen1024 <146147111+luochen1024@users.noreply.github.com>
2025-06-28 17:14:05 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,22 @@
 module.exports = {
-  publicPath: "./",
+  publicPath: './',
   css: {
     loaderOptions: {
       less: {
-        javascriptEnabled: true,
-      },
-    },
+        javascriptEnabled: true
+      }
+    }
   },
   lintOnSave: false,
-  transpileDependencies: ["chart.js"],
-};
+  devServer: {
+    proxy: {
+      '/api': {  // 代理所有以 `/api` 开头的请求
+        target: 'http://localhost:8080', // 替换为实际后端地址（如 http://localhost:3000）
+        changeOrigin: true, // 允许跨域
+        pathRewrite: {
+          '^/api': '' // 移除请求路径中的 `/api` 前缀（按需调整）
+        }
+      }
+    }
+  }
+}
